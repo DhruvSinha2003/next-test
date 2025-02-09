c7c96c10e56e50ab988f92b149ece896a13125e9
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-02-09 20:10:00 +0530

    api

---
+++ app/(api)/db.ts
@@ -0,0 +1,14 @@
+interface Book {
+    id: number;
+    name: string;
+}
+
+const books: Book[] = [
+    { id: 1, name: "To Kill a Mockingbird" },
+    { id: 2, name: "1984" },
+    { id: 3, name: "The Great Gatsby" },
+    { id: 4, name: "The Catcher in the Rye" },
+    { id: 5, name: "Moby-Dick" }
+];
+
+export default books;
