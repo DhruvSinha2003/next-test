2c81ad6fe4a67e588327a52ce15cf88673cd6a47
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-02-09 19:36:20 +0530

    fetch

--- app/(dashboard)/dashboard/analytics/page.tsx
+++ app/(dashboard)/dashboard/analytics/page.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 
-export default function page() {
-  return <div>analytics</div>;
+export default async function page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/albums");
+  if (!res.ok) {
+    throw new Error("Error fetching data");
+  }
+
+  type Album = { id: number; userid: number; title: string };
+
+  const albums = await res.json();
+  return (
+    <div>
+      <h1>Analytics</h1>
+      {albums.map((album: Album) => {
+        return (
+          <div key={album.id}>
+            <h1>Title: {album.title}</h1>
+            <h1>User: {album.userid}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
